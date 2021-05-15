3ed699b4f05f5da4f92d5b8a2229a364cb414fc6
mbeatrice1 <mahorobeatrice9@gmail.com>
2021-05-15 21:50:49 +0200

    removed the virtual

--- theSmartChurch/views.py
+++ theSmartChurch/views.py
@@ -12,6 +12,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializer import ProfileSerializer,ProjectSerializer
 from django.shortcuts import render
+import six
 
 # Create your views here.
 
