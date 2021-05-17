fe4319eabff68d4ff55b88ebf241f19e674f881e
Rachel Uwamahoro <racheluwamahoro55@gmail.com>
2021-05-17 10:21:18 +0200

    configuratingn the view file

--- theSmartChurch/views.py
+++ theSmartChurch/views.py
@@ -21,6 +21,9 @@ def index(request):
     return render(request, 'index.html',{'projects':projects})
 
 
+def bookwedding(request):
+    projects = Project.objects.all().order_by('-date_posted')
+    return render(request, 'book_wedding.html',{'projects':projects})
 
 
 def register(request):
