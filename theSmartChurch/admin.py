3b18d1021058cb67234366fe0a80a9bc3466dfa0
Floride125-art <FTuyisenge120@daviscollege.com>
2021-05-20 23:44:08 +0200

    worked on admin

--- theSmartChurch/admin.py
+++ theSmartChurch/admin.py
@@ -6,9 +6,10 @@ from .models import Item
 
 class MyModelAdmin(AdminVideoMixin, admin.ModelAdmin):
     pass
+from .models import Profile,Project, Announcements
 
 admin.site.register(Item, MyModelAdmin)
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Project)
-# admin.site.register(Announcements)
+admin.site.register(Announcements)
