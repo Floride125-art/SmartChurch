01747267ac4f2306041275ce703fb581ae3f972c
mbeatrice1 <mahorobeatrice9@gmail.com>
2021-05-23 20:09:45 +0200

    added codes

--- smartChurch/settings.py
+++ smartChurch/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
-import django_heroku
+# import django_heroku
 import dj_database_url
 from decouple import config,Csv
 
@@ -162,4 +162,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Configure Django App for Heroku.
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
